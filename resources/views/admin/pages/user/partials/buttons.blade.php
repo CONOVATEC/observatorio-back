df16331970c2f201ecac2f9046ee4f4c94f8f1c6
mkevyn <kevyn_94@outlook.com>
2022-06-15 15:34:14 -0500

    Primera parte de usuarios

---
+++ resources/views/admin/pages/user/partials/buttons.blade.php
@@ -0,0 +1,9 @@
+<div class="d-flex align-items-center col-actions text-center">
+    <a class="dropdown-item" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar">
+        <i class="fa-solid fa-pen-to-square font-medium-2  text-primary"></i>
+    </a>
+    <button href="#" class="dropdown-item deleteConfirm" type="submit" data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar ">
+        <i class="fa-solid fa-trash-can font-medium-2 text-danger"></i>
+    </button>
+
+</div>
