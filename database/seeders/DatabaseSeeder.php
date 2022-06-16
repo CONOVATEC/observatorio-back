0834401de5ba808dd6a27efaf90762917184f0a9
sergio jara <sergiojosel.j@gmail.com>
2022-06-15 20:47:37 -0500

    arreglando errores de duplicados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,36 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-
-      Storage::deleteDirectory('public/news');
+         Storage::deleteDirectory('public/news');
         Storage::makeDirectory('public/news');
-
         \App\Models\User::factory(100)->create();
-
         \App\Models\admin\Category::factory(100)->create();
-
         \App\Models\admin\Tag::factory(10)->create();
-    }
-}
-
         \App\Models\admin\AboutCmpj::factory(10)->create();
         \App\Models\admin\Like::factory(2)->create();
         \App\Models\admin\TypeLogo::factory(10)->create();
         \App\Models\admin\Logo::factory(10)->create();
-
         \App\Models\admin\ReaderLog::factory(20)->create();
         \App\Models\admin\Setting::factory(1)->create();
-        
         $this->call(PostSeeder::class);
         \App\Models\admin\TypeTraining::factory(10)->create();
         \App\Models\admin\Training::factory(10)->create();
         \App\Models\admin\YouthObservatory::factory(10)->create();
         \App\Models\admin\YouthPolicy::factory(10)->create();
         \App\Models\admin\YouthStrategy::factory(10)->create();
-        \App\Models\admin\YouthStrategy::factory(10)->create();
 
     }
-    }
+}
+
+
+
+
+
 
 
