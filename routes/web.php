418ae826608707911f0f0c77682a97e22483eae7
Hector Gomez <hdgomez8@hotmail.com>
2022-06-28 18:56:29 -0500

    PolicyController

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,8 @@ Route::group(['middleware' => 'auth:sanctum', 'verified'], function () {
     Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index');
     Route::get('/usuarios/perfil', [UserController::class, 'profile'])->name('usuarios.perfil');
     Route::resource('noticias', NewController::class)->names('noticias');
-
+    //Politicas
+    Route::resource('politicas', PolicyController::class)->names('politicas');
     // para restaurar categoría
     Route::get('categorias/eliminar-definitivo/{id}', [CategoryController::class, 'deleteDefinitive'])->name('categorias.eliminar.definitivo');
     Route::get('categorias/restaurar/{id}', [CategoryController::class, 'restore'])->name('categorias.restaurar');
